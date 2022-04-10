460b89b6c6793d5616de4bea404152b6612aad07
Ашот агабеков <agabekov.aa@esphere.ru>
2022-04-10 17:39:05 +0300

    update

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ export PROJECT_ABSOLUTE_PATH=/Users/asher/IdeaProjects/ipr-rabbit
 
 #удалить все остановленные старые контейнеры
 docker container prune
-#поднять все сервисы для экстренного пересоздания задать --force-recreate
+#поднять все сервисы, для экстренного пересоздания задать --force-recreate
 docker-compose up -d
